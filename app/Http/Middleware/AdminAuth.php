f93cd31d67747a7babe79dc5c749a6ba3055c56b
Taofeeqoh Billyhadiat <billyhadiattaofeeq@gmail.com>
2024-06-18 00:18:00 +0100

    git push

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminAuth
@@ -16,7 +17,7 @@ class AdminAuth
     public function handle(Request $request, Closure $next)
     {
         if (!Auth::check()) {
-            return redirect()->route('admin.login');
+            return redirect()->route('login');
         }
 
         return $next($request);
